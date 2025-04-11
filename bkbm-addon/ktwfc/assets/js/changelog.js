a26eef9aa5f5ef05ac3279fafa89ca0b72d969a3
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2025-04-11 08:12:02 +0300

    🔥 Updated: Addon documentation changelog scripts.

---
+++ bkbm-addon/ktwfc/assets/js/changelog.js
@@ -0,0 +1,71 @@
+$(function () {
+  // Update the log text
+  function updatesLogTag(string) {
+    let Obj = {
+      "Added:": "<b>🚀 Added:</b>",
+      "Updated:": "<b>🔥 Updated:</b>",
+      "Fixed:": "<b>✅ Fixed:</b>",
+      "Removed:": "<b>❌ Removed:</b>",
+    }
+
+    return string.replace(/Added:|Updated:|Fixed:|Removed:/gi, function (matched) {
+      return Obj[matched]
+    })
+  }
+
+  // Generate Dynamic Log File.
+  function generateDynamicLog(data) {
+    const $dynamic_log = $("#dynamic_log")
+    const commonLogText = "<b>🔥 Updated:</b> Plugin translation file & documentation."
+    if ($dynamic_log.length) {
+      data.forEach((element) => {
+        var outputHtml = "<div>"
+
+        outputHtml += `<p class="log_date">` + element[0] + `</p>`
+
+        if (element[1].length > 0) {
+          // console.log(element[1])
+
+          outputHtml += `<ul class="square-list">`
+
+          element[1].forEach((log) => {
+            outputHtml += `<li>` + updatesLogTag(log) + `</li>`
+          })
+
+          outputHtml += `<li>` + commonLogText + `</li>`
+          outputHtml += `</ul>`
+        }
+
+        outputHtml += "</div>"
+
+        $dynamic_log.before(outputHtml)
+      })
+    } else {
+      alert("#dynamic_log is missing. Example: srcare doc")
+    }
+  }
+
+  // Update Version and Last Update Info
+
+  function updateMetaInfo(metaData) {
+    const $lastUpdate = $(".update"),
+      $ver = $(".ver")
+
+    if ($lastUpdate.length) {
+      $ver.html("").html(metaData[0])
+      $lastUpdate.html("").html(metaData[1])
+    } else {
+      alert(".update is missing. Example: srcare doc")
+    }
+  }
+  // Change only the version and date
+  var metaData = ["1.0.4", "11/04/2025"]
+  updateMetaInfo(metaData)
+
+  // Just add a new row.
+  var changeLogData = [
+    // Add a new row below this comment.
+    ["2018, January, 05 - v 1.0.4", ["Fixed: Plugin styling issues."]],
+  ]
+  generateDynamicLog(changeLogData)
+})
